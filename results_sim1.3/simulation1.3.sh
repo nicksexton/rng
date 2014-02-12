f84d5ec497809d15b22deb38329753c9d78f9a51
Nick Sexton <nick.sexton@gmail.com>
2014-02-12 15:12:42 +0000

    new simulation script (variables)

--- results_sim1.3/simulation1.3.sh
+++ results_sim1.3/simulation1.3.sh
@@ -23,76 +23,81 @@
 echo '---	rate	R	RNG	RG	CS1	CS2	CST	Associates' >> simulation1_summary.txt
 echo 'R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> simulation1_data.txt
 
+UPDATING = 1.0
+DECAY = 40
+MONITORING = 0.09
+TEMPERATURE = 10
+
 
 # echo 'manipulating generation rate (interval = 40c ~ 3.0s)' >> simulation1_summary.txt
-../rng -g 40 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 40 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 cat output_log.dat >> simulation1_data.txt
 
 
 #echo 'manipulating generation rate (interval = 36c ~ 3.0s)' >> simulation1_summary.txt
-../rng -g 36 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 36 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
-#../rng -g 33 -u 0.98 -d 25 -m 0.11 -t 92 
+../rng -g 33 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
 cat output_log.dat >> simulation1_data.txt
 
 
 #echo 'manipulating generation rate (interval = 32c ~ 3.0s)' >> simulation1_summary.txt
-../rng -g 32 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 32 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 #echo 'manipulating generation rate (interval = 28c ~ 3.0s)' >> simulation4_summary.txt
-../rng -g 28 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 28 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
-../rng -g 26 -u 0.98 -d 25 -m 0.11 -t 92 
+../rng -g 26 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
 cat output_log.dat >> simulation1_data.txt
 
 
 
 
 #echo 'manipulating generation rate (interval = 24c ~ 3.0s)' >> simulation1_summary.txt
-../rng -g 24 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 24 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 
 
 
 #echo 'manipulating generation rate (interval = 20c ~ 3.0s)' >> simulation1_summary.txt
-../rng -g 20 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 20 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 
-../rng -g 18 -u 0.98 -d 25 -m 0.11 -t 92 
+../rng -g 18 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
 cat output_log.dat >> simulation1_data.txt
 
 
 #echo 'manipulating generation rate (interval = 16c ~ 1.0s)' >> simulation1_summary.txt
-../rng -g 16 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 16 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 
 
 
 #echo 'manipulating generation rate (interval = 12c ~ 0.5s)' >> simulation1_summary.txt
-../rng -g 13 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 13 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 
-../rng -g 11 -u 0.98 -d 25 -m 0.11 -t 92 
+../rng -g 11 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
 cat output_log.dat >> simulation1_data.txt
 
 
 #echo 'manipulating generation rate (interval = 8c ~ 0.5s)' >> simulation1_summary.txt
-../rng -g 8 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 8 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 #echo 'manipulating generation rate (interval = 6c ~ 0.5s)' >> simulation1_summary.txt
-../rng -g 6 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 6 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 
 
 #echo 'manipulating generation rate (interval = 4c ~ 0.5s)' >> simulation1_summary.txt
-../rng -g 4 -u 0.98 -d 25 -m 0.11 -t 92 >> simulation1_summary.txt
+../rng -g 4 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE >> simulation1_summary.txt
 cat output_log.dat >> simulation1_data.txt
 
 
