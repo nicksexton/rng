b0c814525e727fc2702902e21e2da90e1f654ca3
Nick Sexton <nick.sexton@gmail.com>
2014-02-13 16:53:44 +0000

    tweaked simulation script

--- results_sim1.3/simulation1.3.sh
+++ results_sim1.3/simulation1.3.sh
@@ -20,8 +20,7 @@ SUMMARYDATA="simulation1_summary.txt"
 TRIALSDATA="simulation1_data.txt"
 ZSCORES="simulation1_summary_zscores.txt" # for plotting main DV graphs 
 
-
-echo '---	rate	R	RNG	RG	CS1	CS2	CST	Associates' >> $SUMMARYDATA
+echo 'stat	rate	R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> $SUMMARYDATA
 echo 'R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> $TRIALSDATA
 echo 'stat	rate	R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> $ZSCORES
 
