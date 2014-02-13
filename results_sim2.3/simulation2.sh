b25d181f6bf454ada6bd52abb1aaede4d99b1b16
Nick Sexton <nick.sexton@gmail.com>
2014-02-13 13:00:32 +0000

    better simulation script

--- results_sim2.3/simulation2.sh
+++ results_sim2.3/simulation2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# RNG simulation 2
+# RNG simulatiosn 2 & 3
 # individually varying schema selection weights, otherwise like sim2
 
 # all parameter settings same a
@@ -14,9 +14,15 @@
 # used for graphs (rng_graphs_main.R)
 # Z-scores and SDs need to be processed using 'data - simulations3.ods' spreadsheet
 
+SUMMARYDATA="simulation2_summary.txt"
+TRIALSDATA="simulation2_data.txt"
+ZSCORES="simulation2_summary_zscores.txt" # for plotting main DV graphs 
 
-echo '---	rate	R	RNG	RG	CS1	CS2	CST	Associates' >> simulation2_summary.txt
-echo 'R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> simulation2_data.txt
+
+echo '---	rate	R	RNG	RG	CS1	CS2	CST	Associates' >> $SUMMARYDATA
+echo 'R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> $TRIALSDATA
+echo 'stat	rate	R	RNG	RG	CS1	CS2	CST	A-9	A-8	A-7	A-6	A-5	A-4	A-3	A-2	A-1	A0	A1	A2	A3	A4	A5	A6	A7	A8	A9' >> $ZSCORES
+
 
 UPDATING=1.0
 DECAY=0.0
@@ -25,52 +31,52 @@ TEMPERATURE=10
 
 
 
-../rng -g 40 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
-cat output_log.dat >> simulation2_data.txt
+../rng -g 40 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
+cat output_log.dat >> $TRIALSDATA
 
 
-../rng -g 36 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 36 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
 
 ../rng -g 34 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
-cat output_log.dat >> simulation2_data.txt
+cat output_log.dat >> $TRIALSDATA
 
 
-../rng -g 32 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 32 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
 ../rng -g 29 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE 
-cat output_log.dat >> simulation2_data.txt
+cat output_log.dat >> $TRIALSDATA
 
-../rng -g 28 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
-
-
-../rng -g 24 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 28 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
-../rng -g 23 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
-cat output_log.dat >> simulation2_data.txt
+../rng -g 24 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
+
+
+../rng -g 23 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
+cat output_log.dat >> $TRIALSDATA
 
 
 
-../rng -g 22 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 22 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
-../rng -g 20 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 20 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
-../rng -g 18 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
-cat output_log.dat >> simulation2_data.txt
+../rng -g 18 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
+cat output_log.dat >> $TRIALSDATA
 
 
-../rng -g 16 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 16 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
-../rng -g 14 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
+../rng -g 14 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
 
 
-../rng -g 12 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> simulation2_summary.txt
-cat output_log.dat >> simulation2_data.txt
+../rng -g 12 -u $UPDATING -d $DECAY -m $MONITORING -t $TEMPERATURE | awk 'NF' >> $SUMMARYDATA
+cat output_log.dat >> $TRIALSDATA
 
 
 
